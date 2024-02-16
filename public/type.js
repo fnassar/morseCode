c99978ff4dc5dc776aa9c68b0efdfda57edd791a
fnassar <fan6236@nyu.edu>
2024-02-16 16:42:28 +0400

    push

--- public/type.js
+++ public/type.js
@@ -2,12 +2,12 @@ window.addEventListener("load", () => {
     document
         .querySelector('textarea[name="post"]')
         .addEventListener("input", function () {
-            var remaining = 300 - this.value.length;
+            var remaining = this.value.length;
             if (remaining < 0) {
                 remaining = 0;
             }
             document.getElementById("charRemaining").textContent =
-                "min " + remaining + " characters";
+                remaining + "/ 1000 characters";
         });
 
     let form = document.getElementById("input-form");
@@ -18,6 +18,17 @@ window.addEventListener("load", () => {
         let post = document.getElementById("post").value;
         let readOut = document.getElementById("readOut");
         let isReadOut = readOut.checked; // This will be either true or false
+
+        if (post.length < 300) {
+            // loop text to make 300 characters
+            let loop = 300 / post.length;
+            for (let i = 0; i < loop; i++) {
+                post += post;
+                if (post.length >= 400) {
+                    break;
+                }
+            }
+        }
         console.log("isReadOut", isReadOut);
 
         if (!form.checkValidity()) {
